0067b723c4ea1f9192d076f51fe77a911df35152
Justin <jmms5757@gmail.com>
2024-08-26 00:54:08 -0400

    updated starter code

--- CodeReviewer/Models/ProgrammingLanguages.cs
+++ CodeReviewer/Models/ProgrammingLanguages.cs
@@ -4,8 +4,8 @@ public static class ProgrammingLanguages {
 
     public static string GetStartingCode(ProgrammingLanguagesEnum programmingLanguage) {
         return programmingLanguage switch {
-            ProgrammingLanguagesEnum.JavaScript => "function helloWorld() {{\n\tconsole.log('Hello World!');\n}}\n",
-            ProgrammingLanguagesEnum.CSharp => "using System;\n\nclass HelloWorld\n\n\tpublic static void Main(String[] args) {\n\t\tConsole.WriteLine(\"Hello World!);\n\t}\n\n}",
+            ProgrammingLanguagesEnum.JavaScript => "function helloWorld() {\n\tconsole.log('Hello World!');\n}\n",
+            ProgrammingLanguagesEnum.CSharp => "using System;\n\nclass HelloWorld {\n\n\tpublic static void Main(String[] args) {\n\t\tConsole.WriteLine(\"Hello World!);\n\t}\n\n}",
             _ => throw new ArgumentOutOfRangeException(nameof(programmingLanguage), programmingLanguage, null)
         };
     }
@@ -16,6 +16,18 @@ public static class ProgrammingLanguages {
             ProgrammingLanguagesEnum.CSharp => "cs",
             _ => throw new ArgumentOutOfRangeException(nameof(programmingLanguage), programmingLanguage, null)
         };
+    }
+
+    public static ProgrammingLanguagesEnum? GetProgrammingLanguageFromExtension(string extension) {
+        var programmingLanguageLookup = new Dictionary<string, ProgrammingLanguagesEnum> {
+            { "js", ProgrammingLanguagesEnum.JavaScript},
+            { "cs", ProgrammingLanguagesEnum.CSharp}
+        };
+        
+        return programmingLanguageLookup
+            .TryGetValue(extension.ToLower(), out ProgrammingLanguagesEnum programmingLanguage) 
+            ? programmingLanguage 
+            : null;
     }
     
 }
